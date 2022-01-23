9b77ffa86bce6d4b2167e83e90762144291768cf
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-23 00:15:27 +0000

    Third Commit

--- app/src/main/java/com/example/jetcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetcompose.ui.theme.JetComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +27,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard("Android")
+                    MessageCard(Message("Android","Jetpack Compose"))
                 }
             }
         }
     }
 }
 
+data class Message(val author:String,val body:String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+    }
+    // Add a horizontal space between the image and the column
+    Spacer(modifier = Modifier.width(8.dp))
+
+    Column() {
+        Text(text = message.author)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = message.body)
+    }
 }
 
 @Preview(showBackground = true )
 @Composable
 fun DefaultPreview() {
     JetComposeTheme {
-        MessageCard("Android")
+        MessageCard(message = Message("Tahiru","lorem lorem lorem"))
     }
 }
